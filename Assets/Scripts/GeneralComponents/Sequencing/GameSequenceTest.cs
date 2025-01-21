eb1ad4884c362999737315faef9ef9677821c14b
yarnottc7892 <yarnottc7892@my.uwstout.edu>
2025-01-20 18:00:18 -0600

    Day 2 Animation First Draft. Need some touch up, but it is a good start.

--- Assets/Scripts/GeneralComponents/Sequencing/GameSequenceTest.cs
+++ Assets/Scripts/GeneralComponents/Sequencing/GameSequenceTest.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameSequenceTest : MonoBehaviour
 {
+    [SerializeField]
+    protected InputAction startGameAction = null;
+
     // Start is called before the first frame update
     void Start()
     {
         GameStateManager.Instance.State = GameState.Start;
+        startGameAction.performed += StartGame;
     }
 
-    public void StartGame()
+    void OnEnable()
+    {
+        startGameAction.Enable();
+    }
+
+    void OnDisable()
+    {
+        startGameAction.Disable();
+    }
+
+    public void StartGame(InputAction.CallbackContext context)
     {
         if (GameStateManager.Instance.State == GameState.Start)
         {
